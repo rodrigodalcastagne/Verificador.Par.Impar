4efca9be52b96207818b4515fe664d38d467b0a1
Rodrigo <pcandidorodrigo@gmail.com>
2024-01-18 18:07:02 -0300

    verificadorParImpar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
+
